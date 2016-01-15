5779eca092fa32fca3be086ba3cb3c6ac13b53ef
notr3d <lxmrrr@gmail.com>
2016-01-15 17:32:37 +0300

    second half

---
+++ js/script.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+    $('.clients__wrapper').slick({
+      autoplay:true,
+      autoplaySpeed:2000,
+      arrows:false,
+      draggable:true,
+      dots:true,
+      slidesToShow:5,
+      slidesToScroll:5
+    });
+});
